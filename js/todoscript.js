05c479ce5f35933bfa6d529f0b4264f791b7140c
Howard Kiat <buisnesskiat123@gmail.com>
2023-12-13 17:44:33 +0800

    html update

--- js/todoscript.js
+++ js/todoscript.js
@@ -1,7 +1,6 @@
-// Get Elements From Lists
+// Create a "close" button and append it to each list item
 let inputField = document.querySelectorAll(".lists li");
 // console.log(inputField);
-
 let i; 
 for(i = 0; i < inputField.length ; i++) {
     var span = document.createElement("SPAN");
@@ -10,10 +9,25 @@ for(i = 0; i < inputField.length ; i++) {
     span.appendChild(txt);
     inputField[i].appendChild(span);
 }
+
+// Click on a close button to crossover current list item
+
+
+
+
+
+
 //Check Box On Item Lists
+// const list = document.querySelector('ul');
+
+function listCheckboxHandler(evt) {
+    if (evt.target.tagName === 'li') {
+        evt.target.classList.toggle('checked');
+    }
+}
 
 // Add new item to list with "Add" button
-function newElement() {
+function addTask() {
     let li = document.createElement("li");
     let inputValue = document.getElementsByName("newTask")[0].value;
     let t = document.createTextNode(inputValue);
@@ -24,6 +38,7 @@ function newElement() {
         document.querySelector(".lists").appendChild(li);
     }
     document.getElementsByName("newTask").value ="";
+}
 
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
@@ -31,11 +46,20 @@ function newElement() {
     span.appendChild(txt);
     li.appendChild(span);
 
-    for( i = 0; i <close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
+    for (i = 0; i < close.length; i++) {
+    span.onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    
         }
     }
 
+
+
+function deleteCompletedTask() {
+    let completedTasks = document.querySelectorAll('.lists li.checked');
+
+    completedTasks.forEach(task => {
+        task.parentNode.removeChild(task);
+    });
 }
